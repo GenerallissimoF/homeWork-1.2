0bdf002567864542e16b6e93b86897b3dad675dd
GenerallissimoF <95148081+GenerallissimoF@users.noreply.github.com>
2021-12-08 02:10:53 +0300

    все сделал кроме констрейнтов

--- homeWork 1.2/ViewController.swift
+++ homeWork 1.2/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightSection: UIView!
+   
+    @IBOutlet weak var yelowLightSection: UIView!
+    
+    @IBOutlet weak var greenLightSection: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    var redIsLight = false
+    var yellowIsLight = false
+    var greenIsLight = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.setTitle("Start", for: .normal)
+        startButton.titleLabel?.font = UIFont.systemFont(ofSize: 29.0)
+        roundAndDarkElement(element: redLightSection)
+        roundAndDarkElement(element: yelowLightSection)
+        roundAndDarkElement(element: greenLightSection)
     }
-
-
+    
+  
+    @IBAction func startLightChange() {
+        startButton.setTitle("Next", for: .normal)
+        if !redIsLight {
+            makeDarkThe(element: greenLightSection)
+            makeBrighterThe(element: redLightSection)
+            redIsLight = true
+            
+        }
+        else if !yellowIsLight {
+            makeDarkThe(element: redLightSection)
+            makeBrighterThe(element: yelowLightSection)
+            yellowIsLight = true
+        } else {
+            makeDarkThe(element: yelowLightSection)
+            makeBrighterThe(element: greenLightSection)
+            redIsLight = false
+            yellowIsLight = false
+        }
+        
+    }
+    
 }
+func roundAndDarkElement(element: UIView) {
+    element.layer.cornerRadius = element.bounds.height / 2
+    element.alpha = 0.1
+}
+func makeBrighterThe(element: UIView) {
+    element.alpha = 0.9
+}
+func makeDarkThe(element: UIView) {
+    element.alpha = 0.1
+}
+
 
